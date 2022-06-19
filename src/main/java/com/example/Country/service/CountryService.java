fc7740e8bac59eacabdd240701f947b662f34940
clem-kay <ckoomson75@gmail.com>
2022-06-19 17:00:52 +0000

    added some changes

--- src/main/java/com/example/Country/service/CountryService.java
+++ src/main/java/com/example/Country/service/CountryService.java
@@ -5,7 +5,7 @@ import com.example.Country.model.Country;
 import java.util.List;
 
 public interface CountryService {
-    Country create(Country flag) throws IllegalArgumentException;
+    Country create(Country flag);
     List<Country> list(int pageNo, int size);
     List<Country> search(String name);
     Country get(Long id);
